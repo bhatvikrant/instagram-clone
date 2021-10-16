51559ff9088250b77d3f5cfb71bafdccba344f0e
Vikrant Bhat <vik.bhat223@gmail.com>
2021-10-17 00:16:35 +0530

    Header title & favicon updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import "../styles/globals.css";
 import type { AppProps /*, AppContext */ } from "next/app";
+import Head from "next/head";
+
+// GLOBAL STYLES
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Instagram clone by Vikrant Bhat</title>
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,8 @@
-import Head from "next/head";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Instagram clone</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       {/* =============== Header =============== */}
       <Header />
       {/* =============== Feed =============== */}
