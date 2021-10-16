5de9b51dbfd8016202c0c100113f649d0d7d0611
Vikrant Bhat <vik.bhat223@gmail.com>
2021-10-17 00:14:35 +0530

    <Header /> done - tailwind styling - tailwind animation - mobile responsiveness (mobile first)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      coolss
+    <div>
+      <Head>
+        <title>Instagram clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* =============== Header =============== */}
+      <Header />
+      {/* =============== Feed =============== */}
+      {/* =============== Modal =============== */}
     </div>
   );
 }
