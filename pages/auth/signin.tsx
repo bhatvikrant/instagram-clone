1a4f4e263ac035d4769728fc0c936263113ff1c8
Vikrant Bhat <vik.bhat223@gmail.com>
2021-10-17 13:54:03 +0530

    <SignIn /> styling done, fix redirect after login

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,12 +1,17 @@
+import Image from "next/image";
 import { GetServerSideProps } from "next";
 import { BuiltInProviderType } from "next-auth/providers";
 
+// NEXT AUTH
 import {
   ClientSafeProvider,
   getProviders,
   LiteralUnion,
   signIn as signInToProvider,
 } from "next-auth/react";
+
+// COMPONENTS
+import Header from "../../components/Header";
 
 // TS INTERFACES
 interface Props {
@@ -25,13 +30,26 @@ const SignIn: React.FC<Props> = (props) => {
 
   return (
     <div>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name} className="">
-          <button onClick={() => signInToProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <Header />
+      <div className="grid place-items-center p-2">
+        <div className="relative w-80 h-32 cursor-pointer mt-16">
+          <Image src="/full-logo.webp" layout="fill" objectFit="contain" />
         </div>
-      ))}
+        <div className="mt-16">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name} className="">
+              <button
+                onClick={() =>
+                  signInToProvider(provider.id, { callbackUrl: "/" })
+                }
+                className="p-3 bg-blue-500 rounded-lg text-white"
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
