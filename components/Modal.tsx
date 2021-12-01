1cb5d3c6fed126842eb4b1c2cdccc172466b569e
Vikrant Bhat <vik.bhat223@gmail.com>
2021-12-01 17:53:01 +0530

    e.preventDefault

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -79,6 +79,7 @@ const Modal: React.FC = () => {
   };
 
   const addImageToPost = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
     const reader = new FileReader();
     if (e.target.files?.[0]) {
       reader.readAsDataURL(e.target.files[0]);
