58bba912c6d383869a11bac070d560aa3190205c
Vikrant Bhat <vik.bhat223@gmail.com>
2021-12-01 18:08:14 +0530

    cleanup

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -129,7 +129,6 @@ const Modal: React.FC = () => {
           >
             <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
               <div>
-                <input type="file" />
                 {selectedFile ? (
                   <img
                     src={selectedFile as string}
@@ -142,9 +141,7 @@ const Modal: React.FC = () => {
                     type="button"
                     className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer"
                     onClick={() => {
-                      console.log({ filePickerRefCurr: filePickerRef.current });
                       filePickerRef.current?.click();
-                      // document.getElementById("file-upload-input")?.click();
                     }}
                   >
                     <CameraIcon
